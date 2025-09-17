43d0aad2265c991858af3e61f7a3e73c43d208b4
dariansweb <dariansweb@gmail.com>
2025-09-17 13:00:06 -0500

    fix broken csrf invalid absolute url 2 - update csrf api route

--- app/api/csrf/route.ts
+++ app/api/csrf/route.ts
@@ -2,16 +2,21 @@
 import { NextResponse } from "next/server";
 import crypto from "node:crypto";
 
-export async function GET() {
-  const token = crypto.randomBytes(32).toString("hex");
+export async function GET(req: Request) {
+  const token = crypto.randomBytes(16).toString("hex");
 
-  const res = NextResponse.redirect("/login");
+  const baseUrl =
+    req.url ||
+    `${req.headers.get("x-forwarded-proto") || "http"}://${
+      req.headers.get("host") || "localhost:3000"
+    }`;
+  const res = NextResponse.redirect(new URL("/login", baseUrl));
   res.cookies.set("__Host-csrf", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "lax",
     path: "/",
-    maxAge: 600,
+    maxAge: 600, // 10 minutes
   });
 
   return res;
